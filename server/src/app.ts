e9e4b9ee868f86492111f8b1c64b14ac668d8b4c
Ravi Kumar Sahoo <ravikumarsahoo.902@gmail.com>
2026-04-28 12:03:31 +0530

    error handler added

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,14 +1,14 @@
+import dotenv from "dotenv";
+dotenv.config();
+
 import express from "express";
 import type { Request, Response } from "express";
 import cors from "cors";
-import dotenv from "dotenv";
 import interviewRouter from "./features/interview/interview.routes.js";
 import keywordRouter from "./features/keyword-extract/keyword.routes.js";
 import { auth } from "./middleware/auth.middleware.js";
 import { connectToDB } from "./config/db.js";
 import userRouter from "./features/auth/auth.routes.js";
-
-dotenv.config();
 
 const app = express();
 
@@ -27,6 +27,13 @@ app.get("/", (_req: Request, res: Response) => {
 app.use("/api/auth", userRouter);
 app.use("/api/upload-resume", keywordRouter);
 app.use('/api/interview', auth, interviewRouter);
+
+// Error handling middleware
+app.use((err: any, _req: Request, res: Response, _next: any) => {
+  console.error("Error:", err.message || err);
+  const status = err.status || 400;
+  res.status(status).json({ message: err.message || "Internal server error" });
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
